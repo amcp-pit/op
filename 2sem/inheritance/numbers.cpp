370d1fa5a6f3ad727d6e8458d63ab1fe2dba42b7
st001214 <s.pogozhev@spbu.ru>
2026-02-18 11:09:33 +0300

    add numbers v.0.1

---
+++ 2sem/inheritance/numbers.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+class number {
+protected:
+	int value_;
+public:
+	number(int x = 0) : value_(x) {}
+	void setval(int x) { value_ = (x < 0) ? -x : x; }
+	virtual void show() = 0; // pure virtual
+};
+// class number abstract class, because show() is pure virtual
+
+
+class dectype : public number {
+public:
+	dectype(int x=0) : number(x) {}
+	void show() {std::cout << "Decimal: " << std::dec << value_ << std::endl;}
+};
+
+class octtype : public number {
+public:
+	octtype(int x=0) : number(x) {}
+    void show() {std::cout << "Octal: 0" << std::oct << value_ << std::endl;}
+};
+
+
+class hextype : public number {
+public:
+	hextype(int x=0) : number(x) {}
+    void show() {std::cout << "Hex: 0x" << std::hex << value_ << std::endl;}
+};
+
+
+number * factory(){
+	std::cout << "Input integer type (1..3): ";
+	int n;
+	std::cin >> n;
+	number * p = nullptr;
+	switch(n%3){
+	case 1: p = new dectype; break;
+	case 2: p = new octtype; break;
+	default: p = new hextype;
+	}
+	return p;
+}
+
+int main(){
+	// number x(1);
+	dectype a(317);
+	a.show();
+
+	hextype b(250);
+	b.show();
+
+	number * p;
+	int counter = 5;
+	while(counter-- > 0){
+		p = factory();
+		p->setval(rand()%1000);
+		p->show();
+		delete p;
+	}
+
+	return 0;
+}
