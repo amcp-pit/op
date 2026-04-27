d14af9933ce508fcc7dad59533393d9322e7e4d6
st001214 <s.pogozhev@spbu.ru>
2026-04-27 12:52:53 +0300

    add stl samples

--- 2sem/stl-samples/stl-6-algorithm.cpp
+++ 2sem/stl-samples/stl-6-algorithm.cpp
@@ -22,19 +22,19 @@ template <typename T> void print_elements(const T &M, const char *optstr = "") {
 
 void print(int Element) { cout << "x" << Element << ", "; }
 
-void add10(int &Element) { Element += 10; }
+void square(int & Element) { Element *= Element; }
+
+void add10(int & Element) { Element += 10; }
 
 template <typename T> class AddValue {
   T theValue;
-
 public:
-  AddValue(const T &v) : theValue(v) {}
+  AddValue(const T & value) : theValue(value) {}
   void operator()(T &element) { element += theValue++; }
 };
 
 class MeanValue {
   long sum, num;
-
 public:
   MeanValue() : sum(0), num(0) {}
   void operator()(int Element) {
@@ -57,75 +57,52 @@ int main() {
   copy(A.begin(), A.end(), ostream_iterator<int>(cout, " "));
   cout << endl;
 
-/*
   cout << "One more time: ";
   for_each(A.begin(), A.end(), print);
   cout << endl;
   cout << "reversed: ";
   for_each(A.rbegin(), A.rend(), print);
   cout << endl;
-*/
 
-/*
   for_each(A.begin(), A.end(), add10);
-  print_elements(A, "after add10 = ");
-*/
+  print_elements(A, "A = ");
 
-/*
-  for_each(A.begin(), A.end(), AddValue<int>(100));
-  print_elements(A, "after AddValue<int>(100) = ");
-*/
+  for_each(A.begin(), A.end(), AddValue<int>(10));
+  print_elements(A, "A = ");
 
-/*
-  for_each(A.begin(), A.end(), AddValue<int>(-50));
-  print_elements(A, "after AddValue<int>(-50) = ");
+  for_each(A.begin(), A.end(), AddValue<int>(-10));
+  print_elements(A, "A = ");
 
   for_each(A.begin(), A.end(), AddValue<int>(-*A.begin()));
   print_elements(A, "after AddValue<int>(-*A.begin()) = ");
-*/
 
-/*
   MeanValue MV;
   MV = for_each(A.begin(), A.end(), MV);
   cout << "Mean value: " << double(MV) << endl;
   cout << "Mean value: " << MV << endl;
-*/
 
-/*
   list<int> B;
   insert_elements(B, -20, 3);
   print_elements(B, "Container B: ");
-*/
 
-/*
   MV = for_each(B.begin(), B.end(), MV);
   cout << "Mean value A+B: " << double(MV) << endl;
-*/
 
-/*
   MV = for_each(B.begin(), B.end(), MeanValue());
   cout << "Mean value B: " << double(MV) << endl;
-*/
 
-/*
   A[7] = 4;
   A[10] = 4;
   int num = count(A.begin(), A.end(), 4);
   print_elements(A, "Container A: ");
   cout << "Number of elements equal to 4: " << num << endl;
-*/
 
-/*
   num = count_if(A.begin(), A.end(), isEven);
   cout << "Number of elements with even value: " << num << endl;
-*/
 
-/*
   num = count_if(A.begin(), A.end(), bind2nd(greater<int>(), 4));
   cout << "Number of elements greate then 4: " << num << endl;
-*/
 
-/*
   insert_elements(A, -3, 5);
   print_elements(A, "Container A: ");
 
@@ -133,20 +110,16 @@ int main() {
             A.begin(),          // destination
             negate<int>());
   print_elements(A, "A after first transform: ");
-*/
 
-/*
   list<int> C;
   transform(A.begin(), A.end(), front_inserter(C),
             bind2nd(multiplies<int>(), 10));
   print_elements(A, "A after second transform: ");
   print_elements(C, "C after second transform: ");
-*/
 
-/*
   transform(C.begin(), C.end(), ostream_iterator<int>(cout, "_"),
             negate<int>());
   cout << endl;
-*/
+
   return 0;
 }
